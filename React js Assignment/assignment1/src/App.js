cf704abcb647f5b0df57cd17e49a1dbf242a8f7f
Ashwin0204 <35863171+Ashwin0204@users.noreply.github.com>
2019-02-28 20:50:02 +0530

    Add files via upload

---
+++ React js Assignment/assignment1/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import UserOutput from './UserOutput/UserOutput';
+import UserInput from './UserInput/UserInput';
+
+class App extends Component {
+  state = {
+    username: "Ashwin Balaji"
+  }
+
+  usernameHandler = (event) => {
+    this.setState({username: event.target.value});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <UserInput username={this.state.username} changed={this.usernameHandler} />
+          <UserOutput username={this.state.username} />
+          <UserOutput username={this.state.username} />
+          <UserOutput username={this.state.username} />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
